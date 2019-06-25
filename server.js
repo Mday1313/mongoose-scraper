ebc7aceb96c204923a0b0f37063f3c78dba80bd7
Mday1313 <melissaday076@gmail.com>
2019-06-25 12:39:49 -0400

    Scrape complete, next to db

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var cheerio = require("cheerio");
 var exphbs = require("express-handlebars");
 
 // Require all models
-var db = require("./models");
+var db = require("./models/Notes");
 
 var PORT = process.env.PORT || 3000;
 
@@ -30,13 +30,32 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // add web address to scrape
-// app.get("").then(function(response){
 
-// })
+app.get("/scrape", function (req, res) {
+
+  axios.get("https://www.lonelyplanet.com/travel-tips-and-articles")
+    .then(function (response) {
+      var $ = cheerio.load(response.data);
+
+      $("article div a").each(function (i, element) {
+        var result = {};
+
+        result.title = $(this)
+          .children(".card__content").children("h1")
+          .text();
+        result.link = $(this)
+          .attr("href");
+        result.image = $(this)
+          .children("figure").children("img") 
+          .attr("src");
+
+          console.log(result);
+      })
+    })
+})
 
 
 // Start the server
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
-  
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
