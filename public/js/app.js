d59acfad4bb4b35997b3aed9403d815cd221190c
Mday1313 <melissaday076@gmail.com>
2019-06-27 13:08:06 -0400

    updated saved-btn

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@
 var displayArticles = function () {
     $.getJSON("/articles", function (data) {
         for (var i = 0; i < data.length; i++) {
-            $("#articles").append("<div id='artDiv'class='col-4'><img class='art-image' src='" + data[i].image + "'>" + "<h3>" + data[i].title + "</h3>" + "</p><br/><p class='summary'>" + data[i].summary + "</p>" + "<a class='btn btn-secondary' href='https://www.lonelyplanet.com/travel-tips-and-articles" + data[i].link + "' role='button'>Read Article</a>  <a class='btn btn-danger save-btn' id='" + data[i]._id + "'method='POST' role='button'>Save Article</a></div>");
+            $("#articles").append("<div id='artDiv'class='col-4'><img class='art-image' src='" + data[i].image + "'>" + "<h3>" + data[i].title + "</h3>" + "</p><br/><p class='summary'>" + data[i].summary + "</p>" + "<a class='btn btn-secondary' href='https://www.lonelyplanet.com/travel-tips-and-articles" + data[i].link + "' role='button'>Read Article</a>  <a class='btn btn-danger save-btn' id='" + data[i]._id + "'method='POST' onclick='AddButtonClick()' role='button'>Save Article</a></div>");
         }
 
     });
@@ -68,4 +68,8 @@ $(document).on("click", ".save-btn", function () {
         }
     });
 });
-/* <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a>  <a class="btn btn-secondary" href="#" role="button"> Save Article</a></p> */
+
+function AddButtonClick(){ 
+    //change text from add to Update
+    $(".save-btn").text('Saved');
+  }
